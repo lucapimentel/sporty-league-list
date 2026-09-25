76b352e51675097de409bc46187667525379eb9a
Luca Pimentel <luca.pimentel@outlook.com>
2026-09-25 13:37:59 -0300

    Scaffold app with velvet design system and TanStack query

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { PageHeader } from '@lucapimentel/velvet'
+import "@lucapimentel/velvet/styles.css"
+
+function App() {
+
+  return (
+    <main>
+      <PageHeader title="Sports Leagues" />
+    </main>
+  )
+}
+
+export default App
